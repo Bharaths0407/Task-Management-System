f9b2752ccc3888d0b0356f976c00909100fc4cd7
Bharath S <bharaths0407@gmail.com>
2024-10-27 20:53:57 +0530

    Done with redirects to root page from auth, done with profile view and added router from next navigation

--- src/features/auth/components/sign-in-card.tsx
+++ src/features/auth/components/sign-in-card.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import { z } from "zod"
 import Link from "next/link";
 import { FcGoogle } from "react-icons/fc";
@@ -27,7 +29,7 @@ import { useLogin } from "../api/use-login";
 
 
 export const SignInCard = () => {
-    const { mutate } = useLogin();
+    const { mutate, isPending } = useLogin();
 
 
     const form = useForm<z.infer<typeof LoginSchema>>({
@@ -87,7 +89,7 @@ export const SignInCard = () => {
                             </FormItem>
                         )}
                     />
-                    <Button disabled={false} size="lg" className="w-full">
+                    <Button disabled={isPending} size="lg" className="w-full">
                         Login
                     </Button>
                 </form>
@@ -98,7 +100,7 @@ export const SignInCard = () => {
             </div>
             <CardContent className="p-7 flex flex-col gap-y-4">
                 <Button
-                  disabled={false}
+                  disabled={isPending}
                   variant="secondary" 
                   size="lg" 
                   className="w-full"
@@ -107,7 +109,7 @@ export const SignInCard = () => {
                     Login With Google
                 </Button>
                 <Button
-                  disabled={false}
+                  disabled={isPending}
                   variant="secondary" 
                   size="lg" 
                   className="w-full"
