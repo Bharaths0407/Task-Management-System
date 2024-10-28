65d0547d39552a5dedb76134bece2de3d4119011
Bharath S <bharaths0407@gmail.com>
2024-10-28 22:16:13 +0530

    implemented sidebar and navbar

--- src/app/(dashboard)/page.tsx
+++ src/app/(dashboard)/page.tsx
@@ -1,7 +1,6 @@
 import { redirect } from "next/navigation";
 
 import { getCurrent } from "@/features/auth/action";
-import { UserButton } from "@/features/auth/components/user-button";
 
 export default async function Home() {
   const user = await getCurrent();
@@ -12,7 +11,7 @@ export default async function Home() {
 
   return(
     <div>
-      <UserButton/>
+      This is the Home page
     </div>
   );
 
